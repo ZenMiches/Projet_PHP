994af3371aacd7b28a5c2e32fc2d5c9d613488a4
ZenMiches <jaysonferaut@gmail.com>
2024-05-27 16:25:29 +0200

    Prevent SQL Injection

--- models/CommentManager.php
+++ models/CommentManager.php
@@ -4,7 +4,12 @@ include_once "PDO.php";
 function GetOneCommentFromId($id)
 {
   global $PDO;
-  $response = $PDO->query("SELECT * FROM comment WHERE id = $id");
+  $response = $PDO->prepare("SELECT * FROM comment WHERE id = :id ");
+  $response->execute(
+    array(
+      "id" => $id
+    )
+  );
   return $response->fetch();
 }
 
@@ -18,23 +23,29 @@ function GetAllComments()
 function GetAllCommentsFromUserId($userId)
 {
   global $PDO;
-  $response = $PDO->query(
-    "SELECT comment.*, user.nickname "
-      . "FROM comment LEFT JOIN user on (comment.user_id = user.id) "
-      . "WHERE comment.user_id = $userId "
-      . "ORDER BY comment.created_at ASC"
+  $stmt = $PDO->prepare(
+    "SELECT comment.*, user.nickname
+       FROM comment
+       LEFT JOIN user ON comment.user_id = user.id
+       WHERE comment.user_id = :userId
+       ORDER BY comment.created_at ASC"
   );
-  return $response->fetchAll();
+  $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
+  $stmt->execute();
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function GetAllCommentsFromPostId($postId)
 {
   global $PDO;
-  $response = $PDO->query(
-    "SELECT comment.*, user.nickname "
-      . "FROM comment LEFT JOIN user on (comment.user_id = user.id) "
-      . "WHERE comment.post_id = $postId "
-      . "ORDER BY comment.created_at ASC"
+  $stmt = $PDO->prepare(
+    "SELECT comment.*, user.nickname
+       FROM comment
+       LEFT JOIN user ON comment.user_id = user.id
+       WHERE comment.post_id = :postId
+       ORDER BY comment.created_at ASC"
   );
-  return $response->fetchAll();
+  $stmt->bindParam(':postId', $postId, PDO::PARAM_INT);
+  $stmt->execute();
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
--- models/UserManager.php
+++ models/UserManager.php
@@ -4,7 +4,12 @@ include_once "PDO.php";
 function GetOneUserFromId($id)
 {
   global $PDO;
-  $response = $PDO->query("SELECT * FROM user WHERE id = $id");
+  $response = $PDO->prepare("SELECT * FROM user WHERE id = $id");
+  $response->execute(
+    array(
+      "id" => $id
+    )
+  );
   return $response->fetch();
 }
 
@@ -18,8 +23,12 @@ function GetAllUsers()
 function GetUserIdFromUserAndPassword($username, $password)
 {
   global $PDO;
-  $response = $PDO->query("SELECT id, password FROM user WHERE nickname = '$username'");
-  $user = $response->fetch(PDO::FETCH_ASSOC);
+
+  $stmt = $PDO->prepare("SELECT id, password FROM user WHERE nickname = :username");
+  $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+
+  $stmt->execute();
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
   if ($user && $user['password'] === $password) {
     return $user['id'];
