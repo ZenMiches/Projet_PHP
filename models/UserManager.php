4f9042896f3ec9391b41db3fe216a97e8b1d7244
ZenMiches <jaysonferaut@gmail.com>
2024-05-27 15:39:29 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,16 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $response = $PDO->query("SELECT id, password FROM user WHERE nickname = '$username'");
+  $user = $response->fetch(PDO::FETCH_ASSOC);
+
+  if ($user && $user['password'] === $password) {
+    return $user['id'];
+  } else {
+    return -1;
+  }
+}
